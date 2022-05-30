9c4bc3401fe685a1f05e396b7a82908e4fde26db
artemiy.krot <artemiy.krot@gmail.com>
2022-05-30 21:25:21 +0300

    finally, after creating new algorithm

--- string_sum.go
+++ string_sum.go
@@ -2,6 +2,9 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -23,5 +26,54 @@ var (
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-	return "", nil
+	stringValues := strings.Fields(input)
+	noSpaceString := strings.Join(stringValues, "")
+	var prefix string
+
+	if len(noSpaceString) == 0 {
+		return "", errorEmptyInput
+	}
+	// trimming first symbol
+	if string(noSpaceString[0]) == "+" {
+		noSpaceString = strings.TrimLeft(noSpaceString, "+")
+		prefix = "+"
+	} else if string(noSpaceString[0]) == "-" {
+		noSpaceString = strings.TrimLeft(noSpaceString, "-")
+		prefix = "-"
+	}
+
+	// parsing numbers
+	numbers := []string{}
+	splittedArray := strings.Split(noSpaceString, "+")
+	for _, indexOfArray := range splittedArray {
+		numbers = append(numbers, strings.Split(indexOfArray, "-")...)
+	}
+	// generating errors
+	if len(numbers) > 2 {
+		return "", errorNotTwoOperands
+	}
+
+	// parsing expression
+	expression := string(noSpaceString[len(numbers[0])])
+
+	// creating numbers
+	number1, err1 := strconv.Atoi(prefix + numbers[0])
+	number2, err2 := strconv.Atoi(numbers[1])
+
+	// generating errors for incorrect characters in numbers
+	if err1 != nil {
+		return "", fmt.Errorf("letter in first number: ", err1)
+	} else if err2 != nil {
+		return "", fmt.Errorf("letter in second number: ", err2)
+	}
+
+	// calculate expression
+	switch expression {
+	case "+":
+		output = strconv.Itoa(number1 + number2)
+	case "-":
+		output = strconv.Itoa(number1 - number2)
+	}
+	return output, nil
+
 }
